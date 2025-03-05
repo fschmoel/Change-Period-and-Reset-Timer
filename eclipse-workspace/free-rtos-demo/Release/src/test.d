29581e1943424174cd00068a579ef63b5ea978ca
fschmoel <franz.schmoelling@stud.htwk-leipzig.de>
2025-03-05 09:33:31 +0100

    Add files via upload

---
+++ eclipse-workspace/free-rtos-demo/Release/src/test.d
@@ -0,0 +1,52 @@
+src/test.o: ../src/test.cpp \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoCore/wiring.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoCore/binary.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoCore/HardwareSerial.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoCore/Stream.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoCore/Print.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoCore/WString.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/Arduino_FreeRTOS.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/FreeRTOSConfig.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/projdefs.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/portable.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/portmacro.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/mpu_wrappers.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/FreeRTOSVariant.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/Arduino_FreeRTOS.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/task.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/list.h \
+ /home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/timers.h
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoCore/wiring.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoCore/binary.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoCore/HardwareSerial.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoCore/Stream.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoCore/Print.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoCore/WString.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/Arduino_FreeRTOS.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/FreeRTOSConfig.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/projdefs.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/portable.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/portmacro.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/mpu_wrappers.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/FreeRTOSVariant.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/Arduino_FreeRTOS.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/task.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/list.h:
+
+/home/vboxuser/eclipse-workspace_1/eclipse-workspace/ArduinoLibraries/FreeRTOS/timers.h:
